3cdcb6dc520d9c9621b0995f1f0e3a296fa04162
Dmytro Panchenko <panchenkodmytry@gmail.com>
2018-04-15 13:57:23 +0300

    Implemented GetFrames

--- VideoEditor/VE.Web/Services/FfmpegService.cs
+++ VideoEditor/VE.Web/Services/FfmpegService.cs
@@ -1,12 +1,36 @@
-﻿using VE.Web.Contracts;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using VE.Web.Contracts;
 
 namespace VE.Web.Services
 {
     public class FfmpegService : IFfmpegService
     {
+        private const string AppDataFolder = "AppData";
+        private const string FfmpegPath = "Libs/ffmpeg.exe";
+
         public string GetFrame(string inputVideo, int timeInSeconds)
         {
-            throw new System.NotImplementedException();
+            var outputFrame = $"{Path.GetFileNameWithoutExtension(inputVideo)}_{Guid.NewGuid()}.jpeg";
+            var command = $"{FfmpegPath} -ss {timeInSeconds} -i {inputVideo} -frames:v 1 -y {outputFrame} 2>&1";
+
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    FileName = command
+                }
+            };
+
+            p.Start();
+
+            var output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            return outputFrame;
         }
 
         public string Join(string firstVideo, string secondVideo)
